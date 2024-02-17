7f06661f9fe5a452f5b3b44a590f98c4ae561659
Long9725 <develop9725@gmail.com>
2024-02-17 18:16:22 +0900

    <TEST> SQL_DDL secret

--- domains/domain-user-mysql/src/test/java/com/hompo/user/repository/UserMarketingAgreementRepositoryTest.java
+++ domains/domain-user-mysql/src/test/java/com/hompo/user/repository/UserMarketingAgreementRepositoryTest.java
@@ -3,16 +3,22 @@ package com.hompo.user.repository;
 import com.hompo.enums.MarketingCode;
 import com.hompo.user.entity.MySqlUserMarketingAgreement;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 
+import javax.sql.DataSource;
+import java.sql.Statement;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -24,12 +30,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest
 @ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.yaml")
 public class UserMarketingAgreementRepositoryTest {
     @Autowired
     private TestEntityManager entityManager;
 
     @Autowired
     private UserMarketingAgreementRepository repository;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Value("${sql.ddl}")
+    private String sqlDdl;
+
+    @BeforeEach
+    public void setUp() {
+        // SQL DDL 실행
+        jdbcTemplate.execute(sqlDdl);
+    }
 
     @Test
     void findByUserId_ShouldReturnAgreementsForUser() {
